dde349e9c55d06b1b6c58992fdade78c98e92d5a
Muhammad Faizan Khan <mfkhan1994@gmail.com>
2020-11-15 16:07:43 +0100

    Argument parser written for proper inputs

--- devchallenge/__main__.py
+++ devchallenge/__main__.py
@@ -1,53 +1,49 @@
 """
 Main
--------------
+----
 """
+import json
 import argparse
 from devchallenge import __version__
-import devchallenge.punk.punk
+from devchallenge import punk_requests
 
 
-def get_parser():
+def parse_arguments():
     """
     Creates a new argument parser.
     """
-    parser = argparse.ArgumentParser('devchallenge')
-    version = '%(prog)s ' + __version__
-    parser.add_argument('--version', '-v', action='version', version=version)
-    return parser
+    parser = argparse.ArgumentParser("devchallenge")
+    parser.add_argument('--version', '-v', action='version', version='%(prog)s ' + __version__)
 
+    sub_parser = parser.add_subparsers(dest="sub", required=True)
 
-def main(args=None):
-    """
-    Main entry point for your project.
+    by_name = sub_parser.add_parser("byname", help="String beer name information as partial or full match")
+    by_name.add_argument("--name", "-n", type=str, help="Name as string", required=True)
 
-    Args:
-        args : list
-            A of arguments as if they were input in the command line. Leave it
-            None to use sys.argv.
-    """
+    by_id = sub_parser.add_parser("byid", help="Str exact id or ids in (id|id|...) format")
+    by_id.add_argument("--id", "-i", type=str, help="id as string", required=True)
 
-    parser = get_parser()
-    args = parser.parse_args(args)
+    by_id = sub_parser.add_parser("byinterval", help="Get beer brewed in interval")
+    by_id.add_argument("--from-date", "-f", type=str, help="date i.e. 10-11", required=True)
+    by_id.add_argument("--until-date", "-u", type=str, help="date i.e. 10-11", required=True)
 
-    # Put your main script logic here
-    # print('No action defined for devchallenge module!')
-
-    # import json
-    # print(
-    #     json.dumps(devchallenge.punk.punk.get_beer_by_id("193"),
-    #                indent=4))
-
-    # import json
-    # print(
-    #     json.dumps(devchallenge.punk.punk.get_beer_by_name("B "),
-    #                indent=4))
-
-    import json
-    print(
-        json.dumps(devchallenge.punk.punk.get_beer_brewed_in("10-2011", "10-2020"),
-                   indent=4))
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    main()
+    """
+    Main entry point for your project.
+    """
+    args = parse_arguments()
+
+    if args.sub == "byname":
+        print(json.dumps(punk_requests.get_beer_by_name(args.name),
+                         indent=4))
+
+    elif args.sub == "byid":
+        print(json.dumps(punk_requests.get_beer_by_id(args.id),
+                         indent=4))
+
+    elif args.sub == "byinterval":
+        print(json.dumps(punk_requests.get_beer_brewed_in(args.from_date, args.until_date),
+                         indent=4))
